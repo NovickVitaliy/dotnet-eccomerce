78c3aa9c7fa4ee48a56b85ac5abe8d9fc00f9312
Vitalii Novik <novickvitaliy@gmail.com>
2024-10-03 16:01:03 +0300

    feat: create more stored procedures

--- src/services/ProductInventory/ProductInventory.DataAccess/Persistance/Scripts/Script 0002 - Create procedures.sql
+++ src/services/ProductInventory/ProductInventory.DataAccess/Persistance/Scripts/Script 0002 - Create procedures.sql
@@ -42,3 +42,56 @@ BEGIN
     SELECT * FROM Category
 END
 GO;
+
+CREATE PROCEDURE sp_InsertProduct(
+    @name VARCHAR(50),
+    @description VARCHAR(50),
+    @price INT,
+    @stockQuantity INT,
+    @categoryId INT,
+    @supplierId INT
+)
+AS
+BEGIN
+    INSERT INTO Product(Name, Description, Price, StockQuantity, CategoryId, SupplierId)
+    VALUES (@name, @description, @price, @stockQuantity, @categoryId, @supplierId)
+END
+GO;
+
+CREATE PROCEDURE sp_InsertProductDetail(
+    @weight INT,
+    @height INT,
+    @width INT,
+    @length INT,
+    @color VARCHAR(50),
+    @warrantyPeriod TIME
+)
+AS
+BEGIN
+    INSERT INTO ProductDetail (weight, height, width, length, color, warrantyPeriod)
+    VALUES (@weight, @height, @width, @length, @color, @warrantyPeriod)
+END
+GO;
+
+CREATE PROCEDURE sp_InsertProductTagMapping(
+    @productId INT,
+    @productTagId INT
+)
+AS
+BEGIN
+    INSERT INTO ProductTagMapping(productId, productTagId)
+    VALUES (@productId, @productTagId)
+END
+GO;
+
+CREATE PROCEDURE sp_InsertSupplier(
+    @name VARCHAR(50),
+    @contactInfo VARCHAR(50),
+    @address VARCHAR(50)
+)
+AS
+BEGIN
+    INSERT INTO Supplier(Name, ContactInfo, Address)
+    VALUES (@name, @contactInfo, @address)
+END
+GO;
