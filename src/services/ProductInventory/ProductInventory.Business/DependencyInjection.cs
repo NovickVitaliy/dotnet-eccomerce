563d5799c193c34740cf54511b62b2c200399f93
Vitalii Novik <novickvitaliy@gmail.com>
2024-10-05 14:31:32 +0300

    feat: register services

--- src/services/ProductInventory/ProductInventory.Business/DependencyInjection.cs
+++ src/services/ProductInventory/ProductInventory.Business/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using ProductInventory.Business.Services.Conctracts;
+using ProductInventory.Business.Services.Implementations;
 
 namespace ProductInventory.Business;
 
@@ -8,8 +10,11 @@ public static class DependencyInjection
     public static IServiceCollection ConfigureBusinessLayer(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        
-
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductDetailService, ProductDetailService>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<IProductTagService, ProductTagService>();
+        services.AddScoped<ISupplierService, SupplierService>();
         return services;
     }
 }
